b6e887ab743b44fdd28ef630e753f87b1ef50e8a
aries <aries@192.168.0.171>
2024-10-14 14:34:43 -0600

    union del menu a categoria

--- src/main/java/com/mycompany/quedeahuevo2/ctrl_Categoria.java
+++ src/main/java/com/mycompany/quedeahuevo2/ctrl_Categoria.java
@@ -1,14 +1,29 @@
 package com.mycompany.quedeahuevo2;
-import com.mycompany.quedeahuevo2.Categoria;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 /**
  *
  * @author aries
  */
 public class ctrl_Categoria {
-    
+    public boolean guardar (Categoria objeto){
+        boolean respuesta = false;
+        BaseDatos bd = new BaseDatos();
         
-}
+        Connection cnx = bd.getConexion();
+        try {
+            PreparedStatement consulta = cnx.prepareStatement("");///cueri de la bce de datos//
+            consulta.setInt(1,0);
+            consulta.setString(2, objeto.getDescripcion());
+            
+        }catch (SQLException e){
+            System.out.println("Error al Guardar Categoria: " + e);
+        }
+        return false;
+    }
+}    
+
         
         
   
