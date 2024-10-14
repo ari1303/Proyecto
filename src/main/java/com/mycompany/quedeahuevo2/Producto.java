53c43e1a218678e8f2c43e5775fff524190ab9f7
aries <aries@192.168.0.171>
2024-10-13 21:26:36 -0600

    Face1

---
+++ src/main/java/com/mycompany/quedeahuevo2/Producto.java
@@ -0,0 +1,12 @@
+package com.mycompany.quedeahuevo2;
+
+/**
+ *
+ * @author aries
+ */
+public class Producto {
+    //Atributos
+    private int idProductos;
+    ////Aquiva los campos de la vace de datos
+    
+}
---
+++ src/main/java/com/mycompany/quedeahuevo2/ctrl_Categoria.java
@@ -0,0 +1,14 @@
+package com.mycompany.quedeahuevo2;
+import com.mycompany.quedeahuevo2.Categoria;
+import java.sql.Connection;
+/**
+ *
+ * @author aries
+ */
+public class ctrl_Categoria {
+    
+        
+}
+        
+        
+  
