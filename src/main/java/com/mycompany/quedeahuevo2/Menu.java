44fce8a2b91279bcf306b0ad9503d9651f00e729
aries <aries@192.168.0.171>
2024-10-14 11:39:59 -0600

    union de categorias

--- src/main/java/com/mycompany/quedeahuevo2/Menu.java
+++ src/main/java/com/mycompany/quedeahuevo2/Menu.java
@@ -21,6 +21,7 @@ public class Menu extends javax.swing.JFrame {
         
         this.setLayout(null);
         DesktopPane_menu = new JDesktopPane();
+        
         int ancho = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
         int alto = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
         this.DesktopPane_menu.setBounds(0, 0,ancho, (alto -110));
@@ -38,7 +39,6 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         menuUsuario = new javax.swing.JMenu();
         menuNuevoUs = new javax.swing.JMenuItem();
@@ -63,21 +63,6 @@ public class Menu extends javax.swing.JFrame {
         CerrarSesión = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
-        jPanel1.setAutoscrolls(true);
-        jPanel1.setPreferredSize(new java.awt.Dimension(1200, 700));
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1200, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 700, Short.MAX_VALUE)
-        );
 
         menuUsuario.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         menuUsuario.setText("Usuario");
@@ -177,15 +162,11 @@ public class Menu extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addGap(0, 1200, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addGap(0, 700, Short.MAX_VALUE)
         );
 
         pack();
@@ -196,10 +177,9 @@ public class Menu extends javax.swing.JFrame {
     }//GEN-LAST:event_menuBorrarUsuarioActionPerformed
 
     private void menuNuevaCategoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuNuevaCategoriaActionPerformed
-        IntCategoria intcategoria = new IntCategoria();
-        
-        
-        
+        IntCategoria intCategoria = new IntCategoria();
+        DesktopPane_menu.add(intCategoria);
+        intCategoria.setVisible(true);   
     }//GEN-LAST:event_menuNuevaCategoriaActionPerformed
 
     /**
@@ -241,7 +221,6 @@ public class Menu extends javax.swing.JFrame {
     private javax.swing.JMenu CerrarSesión;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem14;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JMenuItem menuActualizarProducto;
     private javax.swing.JMenuItem menuActualizarUsu;
     private javax.swing.JMenuItem menuBorrarUsuario;
